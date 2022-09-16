4ce5b24f303735bbf5cdacd7c845947c947a9e36
Ervin Kacar <kacarervin@gmail.com>
2022-09-16 16:26:25 +0200

    Finish movie pagination in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Dynamic from "./components/Dynamic";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PaginationDynamic from "./components/PaginationDynamic";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dynamic />
+      <PaginationDynamic />
     </div>
   );
 }
